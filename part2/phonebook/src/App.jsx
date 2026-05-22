9ea92e7b4a7ae6af13e9b88a344b4b6de7c0c161
Davide Amadei <davide.amadei97@gmail.com>
2026-05-22 18:10:24 +0200

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import ContactForm from './components/ContactForm.jsx'
+
 
 const Filter = ({filter, setFilter}) => {
   const handleFilterChange = (event) => {
@@ -22,15 +23,20 @@ const ShowContacts = ({persons, filter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promis fulfilled')
+        setPersons(response.data)
+      }) 
+  }, [])
 
   return (
     <div>
